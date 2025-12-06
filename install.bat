1c51a6e133d0dcd9d72bc1eeff16c61a80573405
curegit <37978051+curegit@users.noreply.github.com>
2025-12-07 00:56:53 +0900

    Fix *.bats

--- install.bat
+++ install.bat
@@ -1,7 +1,4 @@
 @echo off
 setlocal
 pushd %~dp0
-for /F "usebackq delims=" %%i in (`dir /A-D /B /S ^| findstr /R "[^\\]*[^\\]\.mte$"`) do copy /Y "%%i" %UserProfile%\AppData\Roaming\Mp3tag\export
-popd
-endlocal
-pause
+for /F "usebackq delims=" %%i in (`dir /A-D /B /S ^| findstr /R "[^\\]*[^\\]\.mte$"`) do copy /Y "%%i" "%UserProfile%\AppData\Roaming\Mp3tag\export"
